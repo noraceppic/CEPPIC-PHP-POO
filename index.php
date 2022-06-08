303bf8564618d4fd446bcbdc9cc044e0652c4f9b
noraceppic <messanora86@gmail.com>
2022-06-08 17:01:14 +0200

    exemple objet

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+class Voiture {
+    public $couleur;
+    public $masse;
+    public $marque;
+    public $vitess = 0;
+
+    public function affecherMessage()
+    {
+        echo "je suis une voiture ";
+    }
+    public function affecherEnergieCinetique() ;
+}
+
+    {
+         return 0.5 * $this ->masse * ($this-> vetesse ** 2);
+    }
+
+$voiture1 = new voiture();
+$voiture1 ->couleur =" rouge";
+$voiture1 -> masse =1200;
+echo $voiture1-> affecherEnergieCinetique();
+var_dump ($voiture1);
