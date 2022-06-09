8573bb9c38398b2c481924681adde2a3ca62a918
noraceppic <messanora86@gmail.com>
2022-06-09 09:40:02 +0200

    ajout fichier classe

---
+++ calsses/Voiture.php
@@ -0,0 +1,17 @@
+<?php
+class Voiture {
+    public $couleur;
+    public $masse;
+    public $marque;
+    public $vitess = 0;
+
+    public function affecherMessage()
+    {
+        echo "je suis une voiture ";
+    }
+    public function affecherEnergieCinetique() ;
+}
+
+    {
+         return 0.5 * $this ->masse * ($this-> vetesse ** 2);
+    }
--- index.php
+++ index.php
@@ -1,20 +1,5 @@
 <?php
-class Voiture {
-    public $couleur;
-    public $masse;
-    public $marque;
-    public $vitess = 0;
-
-    public function affecherMessage()
-    {
-        echo "je suis une voiture ";
-    }
-    public function affecherEnergieCinetique() ;
-}
-
-    {
-         return 0.5 * $this ->masse * ($this-> vetesse ** 2);
-    }
+require_once './calsses/Voiture.php';
 
 $voiture1 = new voiture();
 $voiture1 ->couleur =" rouge";
